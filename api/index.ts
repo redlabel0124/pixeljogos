21750384920b546586745f8cdece71ac28dde088
redlabel0124 <julianomascarenhas13@gmail.com>
2026-09-03 01:29:44 -0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { appRouter } from "../server/routers";
-import { createContext } from "../server/_core/context";
-import { registerStorageProxy } from "../server/_core/storageProxy";
-import { registerOAuthRoutes } from "../server/_core/oauth";
+import { appRouter } from "../server/routers.js";
+import { createContext } from "../server/_core/context.js";
+import { registerStorageProxy } from "../server/_core/storageProxy.js";
+import { registerOAuthRoutes } from "../server/_core/oauth.js";
 
 const app = express();
 
